a65751c6adfff999faad051c9ba48898674c2ec5
JayKowski <jaymaina5@gmail.org>
2020-08-14 00:07:25 +0300

    added capitalize code and ran tests

---
+++ capitalize.js
@@ -0,0 +1,11 @@
+const capitalize = (word) => {
+    const lower = word.toLowerCase();
+    const sliced = lower.slice(1);
+    const upperLetter = lower.charAt(0).toUpperCase();
+    const capWord = upperLetter + sliced;
+
+    return capWord;
+}
+
+export default capitalize;
+// console.log(capitalize("MICROVESE"));
